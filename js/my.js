49e188702c77081cf0b5c7e179aabf4191c55e72
luck <lucklgh@126.com>
2017-10-01 19:48:36 +0800

    边框更新

--- js/my.js
+++ js/my.js
@@ -5,7 +5,5 @@ function PrintPreview(num) {
         document.getElementById('imgpreview').src = "img/model00.png";
     } else if(num === 1){
         document.getElementById('imgpreview').src = "img/model01.png";
-    } else if(num === 2){
-        document.getElementById('imgpreview').src = "img/model02.png";
     }
 }
