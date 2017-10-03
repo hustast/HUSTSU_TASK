d329abc37232e2be5bcc59a06baaccb14f5e0d47
tigerroarm <u201613443@hust.edu.cn>
2017-10-03 14:59:33 +0800

    本地图片裁剪图示

--- js/my.js
+++ js/my.js
@@ -7,3 +7,42 @@ function PrintPreview(num) {
         document.getElementById('imgpreview').src = "img/model01.png";
     }
 }
+
+window.addEventListener('DOMContentLoaded', function () {
+    var image = document.querySelector('#image-upload');
+    var minAspectRatio = 0.5;
+    var maxAspectRatio = 1.5;
+    var cropper = new Cropper(image, {
+        ready: function () {
+            var cropper = this.cropper;
+            var containerData = cropper.getContainerData();
+            var cropBoxData = cropper.getCropBoxData();
+            var aspectRatio = cropBoxData.width / cropBoxData.height;
+            var newCropBoxWidth;
+
+            if (aspectRatio < minAspectRatio || aspectRatio > maxAspectRatio) {
+                newCropBoxWidth = cropBoxData.height * ((minAspectRatio + maxAspectRatio) / 2);
+
+                cropper.setCropBoxData({
+                    left: (containerData.width - newCropBoxWidth) / 2,
+                    width: newCropBoxWidth
+                });
+            }
+        },
+        cropmove: function () {
+            var cropper = this.cropper;
+            var cropBoxData = cropper.getCropBoxData();
+            var aspectRatio = cropBoxData.width / cropBoxData.height;
+
+            if (aspectRatio < minAspectRatio) {
+                cropper.setCropBoxData({
+                    width: cropBoxData.height * minAspectRatio
+                });
+            } else if (aspectRatio > maxAspectRatio) {
+                cropper.setCropBoxData({
+                    width: cropBoxData.height * maxAspectRatio
+                });
+            }
+        }
+    });
+});
